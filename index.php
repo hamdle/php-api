9e9469106bc8a902bbd34dbfdb79ecd6636a16db
Eric Marty <contact@ericmarty.co>
2019-12-30 09:57:31 -0800

    Add ErrorLog utility class.

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
+require_once __DIR__.'/src/Utils/ErrorLog.php';
 
-function var_error_log($object = null, $tag = '') {
-    if ($object == null) {
-        $object = "null";   // be explicit for print_r
-    }
+use Utils\ErrorLog;
 
-    ob_start();
-    print("\n###START $tag\n");
-    print_r($object);
-    print("\n###END $tag\n");
-    $contents = ob_get_contents();
-    ob_end_clean();
-    error_log($contents);
-}
-
-var_error_log($_SERVER['REQUEST_METHOD'], "METHOD###");
-var_error_log($_SERVER['REQUEST_URI'], "URI###");
-var_error_log($_POST, "POST###");
-var_error_log(json_decode(file_get_contents('php://input'), true), "FILE###");
+ErrorLog::print($_SERVER['REQUEST_METHOD'], "METHOD###");
+ErrorLog::print($_SERVER['REQUEST_URI'], "URI###");
+ErrorLog::print($_POST, "POST###");
+ErrorLog::print(json_decode(file_get_contents('php://input'), true), "FILE###");
 
 function send_test_response() {
     $data = [
---
+++ src/Utils/ErrorLog.php
@@ -0,0 +1,21 @@
+<?php
+namespace Utils;
+
+class ErrorLog {
+
+    public static function print($object = null, $tag = '') {
+        if ($object == null) {
+            $object = "null";   // be explicit for print_r
+        }
+        $content = '';
+
+        ob_start();
+        print("\n###START $tag\n");
+        print_r($object);
+        print("\n###END $tag\n");
+        $contents = ob_get_contents();
+        ob_end_clean();
+        error_log($contents);
+    }
+
+}
