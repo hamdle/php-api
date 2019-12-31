827e2428d8717c04ccc9a4b7dd10a9af2b7e1719
Eric Marty <contact@ericmarty.co>
2019-12-30 18:24:59 -0800

    Make the parser work. Needs further revision.
    Got to part (1) of the (1) make it work, (2) make it right,
    (3) make it fast.

--- src/Http/Api.php
+++ src/Http/Api.php
@@ -25,15 +25,41 @@ class Api
     private function getController($request)
     {
         $endpoints = $this->quickFilterEndpoints($request);
-        ErrorLog::print($endpoints);
+        ErrorLog::print($endpoints, 'ENDPOINTS###');
         if (empty($endpoints))
         {
             return new Controller('ErrorController', 'get');
         }
 
+        $requestParts = $request->getPathParts();
+        ErrorLog::print($requestParts, 'REQUEST_PARTS###');
+        $match = true;
         foreach ($endpoints as $key => $value)
         {
-            if ($key == $request->getPathParts()[0])
+            $match = true;
+            $keyParts = explode('/', $key);
+            ErrorLog::print($keyParts, 'KEY_PARTS###');
+            /*if ($key == $requestParts[0])
+            {
+                $parts = explode('.', $value);
+                $class= $parts[0];
+                $method = $parts[1];
+
+                return new Controller($class, $method);
+            }*/
+
+            for ($n = 0; $n < count($requestParts); $n++)
+            {
+                if ($requestParts[$n] == $keyParts[$n])
+                {
+                    continue;
+                }
+                if (intval($requestParts[$n]) === 0)
+                {
+                    $match = false;
+                }
+            }
+            if ($match)
             {
                 $parts = explode('.', $value);
                 $class= $parts[0];
