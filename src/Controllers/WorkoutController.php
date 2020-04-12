8b831a20a8af5af2277e9e65315c36597aca0e1a
Eric Marty <contact@ericmarty.co>
2020-04-12 11:29:13 -0700

    Update workout processing to reflect new keys.

--- src/Controllers/WorkoutController.php
+++ src/Controllers/WorkoutController.php
@@ -37,9 +37,9 @@ class WorkoutController implements ControllerInterface
 
             $entries = new Entries();
             $reps = new Reps();
-            for ($index = 0; $index < count($args['exercises']); $index++) {
-                $entries->add($entries->filter_args($args['exercises'][$index]));
-                $reps->add($reps->filter_args($args['exercises'][$index]));
+            for ($index = 0; $index < count($args['entries']); $index++) {
+                $entries->add($entries->filter_args($args['entries'][$index]));
+                $reps->add($reps->filter_args($args['entries'][$index]['reps']));
             }
 
             $response = new Response();
