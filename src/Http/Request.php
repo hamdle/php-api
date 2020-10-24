2e4e5f9757983a6291018c69ad5f6f62951055d7
Eric Marty <contact@ericmarty.dev>
2020-10-24 16:25:24 -0700

    Refactor Http\Request class.

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -1,77 +1,58 @@
 <?php
+
+/*
+ * Http/Request.php: http request information
+ *
+ * Copyright (C) 2020 Eric Marty
+ */
+
 namespace Http;
 
-class Request {
-    private static $instance;
-
-    public static function get()
-    {
-        if (isset(self::$instance))
-        {
-            return self::$instance;
-        }
-        else
-        {
-            self::$instance = new Request();
-            return self::$instance;
-        }
-    }
-
-    public function getPath()
+class Request
+{
+    public static
+    function uri()
     {
         return $_SERVER['REQUEST_URI'];
     }
 
-    public function getPathParts()
+    public static
+    function pathArr()
     {
-        // TODO Make this better. Use a for loop and in_array to filter out the items.
-        $parts = explode('/', $this->getPath());
-
-        // Remove uri elements that are not part of the API request.
-        $filter = array_filter(
-            $parts,
-            function($item)
+        $arr = array_filter(
+            explode('/', self::uri()),
+            function($part)
             {
-                if (empty(trim($item)))
-                {
+                if (empty(trim($part)) || $part == 'api')
                     return 0;
-                }
-                // TODO Make this use a static array of uri elements to be removed.
-                if ($item == 'api')
-                {
-                    return 0;
-                }
-
                 return 1;
             }
         );
 
-        // Re-index the array to start at index 0
-        $reindexed = [];
-        foreach ($filter as $part)
-        {
-            $reindexed[] = $part;
-        }
-
-        return $reindexed;
+        return array_values($arr);
     }
 
-    public function getMethod()
+    public static
+    function method()
     {
         return $_SERVER['REQUEST_METHOD'];
     }
 
-    public function getData()
+    public static
+    function data()
     {
         return json_decode(file_get_contents('php://input'), true);
     }
 
-    public function getPost()
+    public static
+    function post()
     {
         return $_POST;
     }
 
-    public function getCookie() {
+    public static
+    function cookie()
+    {
         return $_COOKIE;
     }
 }
