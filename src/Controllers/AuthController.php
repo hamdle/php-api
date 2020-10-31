e08cb4f413e35803e4ec59f49da7e1e27bd05fae
Eric Marty <contact@ericmarty.dev>
2020-10-31 08:00:10 +0000

    Refactor AuthController.php.

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -1,4 +1,11 @@
 <?php
+
+/*
+ * Controllers/AuthController.php: authorize user requests
+ *
+ * Copyright (C) 2020 Eric Marty
+ */
+
 namespace Controllers;
 
 use Http\Response;
@@ -7,24 +14,24 @@ use Database\Query\Sessions;
 
 class AuthController implements ControllerInterface
 {
-    public function get($args = [])
+    public function
+    get($args = [])
     {
         $response = new Response();
         $sessions = new Sessions();
 
-        if ($sessions->verify()) {
+        if ($sessions->verify())
             return $response->send(Response::HTTP_200_OK);
-        }
 
         return $response->send(Response::HTTP_401_UNAUTHORIZED);
     }
 
-    public function post($args = [])
+    public function
+    post($args = [])
     {
         $filteredArgs = array_map(function($item) {
-                if (!is_null($item) && array_key_exists('password', $item)) {
+                if (!is_null($item) && array_key_exists('password', $item)) 
                     $item['password'] = md5($item['password']);
-                }
                 return $item;
             },
             $args
@@ -42,17 +49,20 @@ class AuthController implements ControllerInterface
         }
     }
 
-    public function put($args = [])
+    public function
+    put($args = [])
     {
         // TODO
     }
 
-    public function patch($args = [])
+    public function
+    patch($args = [])
     {
         // TODO
     }
 
-    public function delete($args = [])
+    public function
+    delete($args = [])
     {
         // TODO
     }
