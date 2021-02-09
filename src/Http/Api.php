142181691f323925d10f07412221d7f9c0864faa
Eric Marty <contact@ericmarty.dev>
2021-02-08 22:26:54 -0800

    Remove Api's controller function since registration is now automatic

--- src/Http/Api.php
+++ src/Http/Api.php
@@ -62,33 +62,4 @@ class Api
         else
             throw new \Exception("Unable to verify 'post' Api endpoint for token \"".$controller."\".");
     }
-
-    /*
-     * Add post request to the Api.
-     * @param $registrars - array of strings
-     * @return void
-     */
-    public static function controllers($registrars)
-    {
-        try
-        {
-            foreach ($registrars as $registrar)
-            {
-                if (class_exists($registrar))
-                {
-                    $domain = Router::register($registrar::registration());
-                    self::$controllerNamespaces[] = [$domain => $registrar];
-                }
-                else 
-                {
-                    throw new \Exception("Unable to register controller ".$registrar);
-                }
-            }
-        } 
-        catch (\Exception $e)
-        {
-            $response = new Response();
-            $response->sendAndExit(Response::HTTP_400_BAD_REQUEST, $e->getMessage());
-        }
-    }
 }
