13f8b11dc617b49e5bd78766deda7315287e1387
hamdle <ericmarty@protonmail.com>
2021-09-11 13:47:45 -0700

    Make the Api smarter

--- index.php
+++ index.php
@@ -3,34 +3,35 @@
 /*
  * index.php: define the Api here
  *
+ * This framework uses a simple mental model and works like so:
+ *
+ * Endpoints map to functions that handle them in the root Controllers
+ * directory. The Api responds by matching the request to an endpoint and
+ * calling the endpoint's controller function. Use Models, located in the root
+ * Models directory, to interact with the database. Support functions, like
+ * date and debug helpers, can be found in \Core\Utils. And that's about it.
+ * For more details check out the individual file(s) you're interested in.
+ *
  * Copyright (C) 2021 Eric Marty
  */
 
 require __DIR__.'/src/autoload.php';
 
 use Core\Api;
-use Core\Http\Response;
-use Core\Http\Code;
 
-// TODO Update api to accept simpler three argument functions
-//Api::get('version', 'Ping', 'version');
-//Api::get('coffee', 'Ping', 'teapot');
-//Api::get('auth', 'Authentication', 'verifySession');
-//Api::get('exercises', 'Workouts', 'exerciseTypes');
-//Api::get('workouts', 'Workouts', 'allWorkouts');
-//Api::post('login', 'Authentication', 'login');
-//Api::post('workouts/new', 'Workouts', 'save');
-
+Api::get('version', 'Cats', 'verifySession');
+Api::get('auth', 'Authentication', 'verifySession');
+Api::get('exercises', 'Workouts', 'exerciseTypes');
+Api::get('workouts', 'Workouts', 'allWorkouts');
+Api::post('login', 'Authentication', 'login');
+Api::post('workouts/new', 'Workouts', 'save');
+/*
 Api::get('version', function() {
     return Response::send(Code::OK_200, ["version" => $_ENV['VERSION']]);
 });
 Api::get('coffee', function() {
     return Response::send(Code::IM_A_TEAPOT_418, ["message" => "418 I'm a teapot"]);
 });
-Api::get('auth', ['\Controllers\Authentication', 'verifySession']);
-Api::get('exercises', ['\Controllers\Workouts', 'exerciseTypes']);
-Api::get('workouts', ['\Controllers\Workouts', 'allWorkouts']);
-Api::post('login', ['\Controllers\Authentication', 'login']);
-Api::post('workouts/new', ['\Controllers\Workouts', 'save']);
+ */
 
-Api::respond();
+return Api::respond();
--- src/Core/Api.php
+++ src/Core/Api.php
@@ -1,7 +1,11 @@
 <?php
 
 /*
- * Core/Http/Api.php: holder of the ring
+ * Core/Api.php: holder of the ring
+ *
+ * The one class to control them all. The Api maintains an internal $api array
+ * that maps endpoints to controller functions. The Api responds to a request.
+ * And the Api knows how to route a request to an ($api) endpoint.
  *
  * Copyright (C) 2021 Eric Marty
  */
@@ -9,53 +13,101 @@
 namespace Core;
 
 use \Core\Http\Response;
-use \Core\Http\Router;
+use \Core\Http\Request;
+use \Core\Http\Code;
 
 class Api
 {
-    /*
-     * The ring, an array of Api endpoints keyed by request method.
-     * @var array
-     */
+    // The ring, an array of Api endpoints mapped to controllers.
     private static $api;
 
-    /*
-     * Add GET request to the Api.
-     * @return void
-     */
-    public static function get($path, $controller)
-    {
-        self::$api['get'][] = [$path => $controller];
-    }
+    // Change this to put the controllers in a different directory.
+    private static $CONTROLLER_ROOT = "\\Controllers\\";
 
     /*
-     * Add POST request to the Api.
-     * @return void
+     * Public methods
      */
-    public static function post($path, $controller)
+
+    public static function get($endpoint, $controller, $function)
     {
-        self::$api['post'][] = [$path => $controller];
+        self::$api["get"][] = [$endpoint => [$controller, $function]];
     }
 
-    /*
-     * Route the request in to a Controller and execute it to return a response.
-     * @return \Http\Response
-     */
+    public static function post($endpoint, $controller, $function)
+    {
+        self::$api["post"][] = [$endpoint => [$controller, $function]];
+    }
+
+    // Invoke a controller using the request and Api. This is the surface,
+    // uncaught exceptions can bubble up to here.
+    // return = \Core\Http\Response
     public static function respond()
     {
-        $controller = Router::requestToController(self::$api);
+        $controller = self::route(Request::path(), self::$api);
 
-        if (is_callable($controller))
-            return $controller();
-
-        if (is_array($controller) &&
-            count($controller) == 2 &&
-            ($invokedController = [new $controller[0](), $controller[1]]) &&
-            is_callable($invokedController))
+        if (is_null($controller))
         {
-            return $invokedController();
+            return Response::send
+            (
+                Code::NOT_FOUND_404,
+                [
+                    "message" => "404 not found",
+                ]
+            );
         }
 
-        return Response::sendDefault();
+        $namespace = self::$CONTROLLER_ROOT.$controller[0];
+        $function = $controller[1];
+
+        try {
+            if ($instantiatedController = [new $namespace, $function])
+                return $instantiatedController();
+
+            return Response::sendDefault();
+        }
+        catch (\Throwable $e)
+        {
+            return Response::send
+            (
+                Code::INTERNAL_SERVER_ERROR_500,
+                [
+                    "error" => $e->getMessage(),
+                ]
+            );
+        }
+    }
+
+    /*
+     * Priate methods
+     */
+
+    // Given [$endpoint => $controller] match the $endpoint to the request and
+    // return = $controller or null
+    private static function route($path, $api)
+    {
+        // TODO can you reduce the complexity of this?
+        foreach ($api[Request::method()] ?? [] as $route)
+        {
+            foreach ($route as $uri => $controller)
+            {
+                $uriParts = explode("/", $uri);
+                $pass = true;
+
+                for ($i = 0; $i < count($uriParts); $i++)
+                {
+                    if (!(isset($path[$i]) &&
+                        isset($uriParts[$i]) &&
+                        $path[$i] == $uriParts[$i]))
+                    {
+                        $pass = false;
+                    }
+                }
+
+                if ($pass)
+                    return $controller;
+            }
+        }
+
+        return null;
     }
 }
